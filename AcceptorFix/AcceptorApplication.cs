01305fe29ee11e387f50e1b42e52b580e7fd7f8f
BartH28 <henriquelb28@gmail.com>
2022-08-15 13:14:15 -0300

    Acceptor Iniciado com logs em Json

---
+++ AcceptorFix/AcceptorApplication.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickFix;
+using Newtonsoft.Json;
+using QuickFix.Fields;
+
+
+namespace AcceptorFix
+{
+   
+    public class AcceptorApplication : IApplication
+    {
+        //private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(AcceptorApplication));
+        
+        
+
+        public void FromAdmin(Message message, SessionID sessionID)
+        {
+            var msgJson = JsonConvert.SerializeObject(message, Formatting.Indented);
+           
+            log.Info(msgJson.ToString() + " Message from Admin Log4net");
+        }
+
+        public void FromApp(Message message, SessionID sessionID)
+        {
+            var msgJson = JsonConvert.SerializeObject(message, Formatting.Indented);
+            
+            log.Info(msgJson.ToString() + " Message from App Log4net");
+        }
+
+        public void OnCreate(SessionID sessionID)
+        {
+            
+            log.Info("SessionID: " + sessionID);
+        }
+
+        public void OnLogon(SessionID sessionID)
+        {
+            QuickFix.FIX44.NewOrderSingle order = new QuickFix.FIX44.NewOrderSingle(
+            new ClOrdID("1234"),
+            new Symbol("AAPL"),
+            new Side(Side.BUY),
+            new TransactTime(DateTime.Now),
+            new OrdType(OrdType.MARKET));
+
+            Session.SendToTarget(order, sessionID);
+            log.Info("NewOrderSingle send to " + sessionID);
+
+            log.Info(sessionID + " logon");
+        }
+
+        public void OnLogout(SessionID sessionID)
+        {
+            
+            
+            log.Info(sessionID + " logout");
+        }
+
+        public void ToAdmin(Message message, SessionID sessionID)
+        {
+            var msgJson = JsonConvert.SerializeObject(message, Formatting.Indented);
+            log.Info(msgJson.ToString() + " to admin in " + sessionID);
+            
+        }
+
+        public void ToApp(Message message, SessionID sessionId)
+        {
+            var msgJson = JsonConvert.SerializeObject(message, Formatting.Indented);
+            log.Info(msgJson.ToString() + " Message to app Log4net");
+            
+        }
+    }
+}
---
+++ AcceptorFix/Program.cs
@@ -0,0 +1,36 @@
+﻿using QuickFix;
+using QuickFix.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptorFix
+{
+    internal class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+        static void Main(string[] args)
+        {
+                    SessionSettings settings = new SessionSettings(args[0]);
+                    IApplication myApp = new AcceptorApplication();
+                    IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+                    ILogFactory logFactory = new FileLogFactory(settings);
+                    ThreadedSocketAcceptor acceptor = new ThreadedSocketAcceptor(
+                        myApp,
+                        storeFactory,
+                        settings,
+                        logFactory);
+
+                    acceptor.Start();
+
+            log.Info("Acceptor iniciado");
+            
+            Console.WriteLine("press <enter> to quit");
+                    Console.Read();
+                    acceptor.Stop();
+                
+        }
+    }
+}
