40d615936049dee1d6776b492ac5acb582d3b475
BartH28 <henriquelb28@gmail.com>
2022-08-16 16:18:25 -0300

    Add MessageCracker

--- AcceptorFix/AcceptorApplication.cs
+++ AcceptorFix/AcceptorApplication.cs
@@ -11,7 +11,7 @@ using QuickFix.Fields;
 namespace AcceptorFix
 {
    
-    public class AcceptorApplication : IApplication
+    public class AcceptorApplication : MessageCracker,IApplication
     {
         //private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(AcceptorApplication));
@@ -20,6 +20,16 @@ namespace AcceptorFix
 
         public void FromAdmin(Message message, SessionID sessionID)
         {
+            try
+            {
+                Crack(message, sessionID);
+            }
+            catch (Exception ex)
+            {
+                log.Info("Error: " + ex);
+                
+            }
+            
             var msgJson = JsonConvert.SerializeObject(message, Formatting.Indented);
            
             log.Info(msgJson.ToString() + " Message from Admin Log4net");
@@ -27,6 +37,7 @@ namespace AcceptorFix
 
         public void FromApp(Message message, SessionID sessionID)
         {
+           
             var msgJson = JsonConvert.SerializeObject(message, Formatting.Indented);
             
             log.Info(msgJson.ToString() + " Message from App Log4net");
@@ -40,15 +51,6 @@ namespace AcceptorFix
 
         public void OnLogon(SessionID sessionID)
         {
-            QuickFix.FIX44.NewOrderSingle order = new QuickFix.FIX44.NewOrderSingle(
-            new ClOrdID("1234"),
-            new Symbol("AAPL"),
-            new Side(Side.BUY),
-            new TransactTime(DateTime.Now),
-            new OrdType(OrdType.MARKET));
-
-            Session.SendToTarget(order, sessionID);
-            log.Info("NewOrderSingle send to " + sessionID);
 
             log.Info(sessionID + " logon");
         }
@@ -62,6 +64,15 @@ namespace AcceptorFix
 
         public void ToAdmin(Message message, SessionID sessionID)
         {
+            try
+            {
+                Crack(message, sessionID);
+            }
+            catch (Exception ex)
+            {
+                log.Info("Error: " + ex);
+
+            }
             var msgJson = JsonConvert.SerializeObject(message, Formatting.Indented);
             log.Info(msgJson.ToString() + " to admin in " + sessionID);
             
@@ -72,6 +83,28 @@ namespace AcceptorFix
             var msgJson = JsonConvert.SerializeObject(message, Formatting.Indented);
             log.Info(msgJson.ToString() + " Message to app Log4net");
             
+        }
+
+        public void OnMessage(QuickFix.FIX44.NewOrderSingle ord,SessionID sessionID)
+        {
+            ProcessOrder(ord.OrderQty, ord.Account);
+            
+        }
+
+        public void OnMessasge(QuickFix.FIX44.Logon logonMsg)
+        {
+            var msgJson = JsonConvert.SerializeObject(logonMsg, Formatting.Indented);
+            log.Info(msgJson);
+        }
+
+        public void OnMessasge(QuickFix.FIX44.Logout logoutMsg)
+        {
+            var msgJson = JsonConvert.SerializeObject(logoutMsg, Formatting.Indented);
+            log.Info(msgJson);
+        }
+        protected void ProcessOrder(OrderQty quantity,Account account)
+        {
+            log.Info(" Quantidade: " + quantity + " Conta: " + account);
         }
     }
 }
